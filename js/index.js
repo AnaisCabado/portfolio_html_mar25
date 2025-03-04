f2758ad929d33394cd2b19d404c50465a7625dcf
Anaís <acabado001ehu@gmail.com>
2025-03-04 15:45:50 +0100

    estructura final

---
+++ js/index.js
@@ -0,0 +1,25 @@
+// FETCH HEADER
+fetch("../fetch/header.html")
+    .then(response => response.text())
+    .then(data => {
+        document.querySelector("header").innerHTML = data;
+    })
+    .catch(error => {
+        console.error("Error al cargar el header:", error);
+    });
+
+// DISPLAY NAV
+function toggleNav() {
+    let nav = document.querySelector(".nav-apartados");
+    nav.classList.toggle("active");
+}
+
+// FETCH FOOTER
+fetch("../fetch/footer.html")
+    .then(response => response.text())
+    .then(data => {
+        document.querySelector("footer").innerHTML = data;
+    })
+    .catch(error => {
+        console.error("Error al cargar el footer:", error);
+    });
